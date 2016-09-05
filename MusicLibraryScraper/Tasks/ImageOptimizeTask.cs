edaaf08d005394b9f3ebe774f32451cbc3d975d2
Justin Robb <reptarsrage@gmail.com>
2016-09-05 00:54:56 -0700

    Separated conjoined tasks out into different files.

--- MusicLibraryScraper/Tasks/ImageOptimizeTask.cs
+++ MusicLibraryScraper/Tasks/ImageOptimizeTask.cs
@@ -1,78 +1,8 @@
 ﻿namespace MusicLibraryScraper.Tasks
 {
     using Managers;
-    using System;
-    using System.Drawing;
     using System.IO;
-    using System.Runtime.InteropServices;
     using System.Threading.Tasks;
-
-    class ImageOptimizeWithoutFileTask : IDisposable
-    {
-        public ConcurrentImageTask Task;
-        private MemoryStream stream;
-        private Image image;
-        private object lockMe = new object();
-
-        public Image OptimizeImage(Image image)
-        {
-            
-            var _imageMan = new ImageManager();
-            using (var resizedImage = _imageMan.ScaleImage(image, 600))
-            {
-                long newSize = 0;
-                //var outImage = _imageMan.ConvertImagetoQuality(resizedTest, 90, out newSize);
-                this.stream = new MemoryStream();
-                var outImage = _imageMan.ConvertImagetoQuality(resizedImage, 90, out newSize, ref stream);
-                Logger.AddOptimizedImageSize(newSize);
-                return outImage;
-            }
-        }
-
-        public void Dispose()
-        {
-            try
-            {
-                this.image.Dispose();
-            }
-            catch { }
-
-            try
-            {
-                this.stream.Dispose();
-            }
-            catch { }
-
-            try
-            {
-                this.Task.Dispose();
-            }
-            catch { }
-        }
-
-        public ImageOptimizeWithoutFileTask(Image image)
-        {
-            this.Task = new ConcurrentImageTask(() => OptimizeImage(image));
-        }
-
-        public Image Result
-        {
-            get
-            {
-                lock (lockMe)
-                {
-                    if (this.Task.Result != null)
-                    {
-                        return (Image)this.Task.Result.Clone();
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-            }
-        }
-    }
 
     class ImageOptimizeTask : Task<FileInfo>
     {
